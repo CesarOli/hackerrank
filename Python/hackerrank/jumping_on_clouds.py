25d489909f6b796a231a2f3ba192633998d28f33
CesarOli <cesaraleoliveira@gmail.com>
2024-07-01 23:23:27 -0300

    chore: add initial structure for jumping on clouds challenge.

---
+++ Python/hackerrank/jumping_on_clouds.py
@@ -0,0 +1,23 @@
+from time import sleep
+
+def statement():
+
+    return
+
+def jumping_on_clouds(c):
+    
+    return
+
+
+if __name__ == '__main__':
+
+    statement()
+
+    n = int(input().strip())
+
+    c = list(map(int, input().rstrip().split()))
+
+    sleep(2)
+
+    result = jumping_on_clouds(c)
+
