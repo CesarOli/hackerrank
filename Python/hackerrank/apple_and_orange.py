0581f8fb07f073687a4767b462a18a76f5639632
CesarOli <cesaraleoliveira@gmail.com>
2024-07-13 17:11:44 -0300

    feat: Implement countApplesAndOranges function to count fruit within range.

--- Python/hackerrank/apple_and_orange.py
+++ Python/hackerrank/apple_and_orange.py
@@ -1,29 +1,27 @@
 
 def countApplesAndOranges(s, t, a, b, apples, oranges):
-    return
+    
+    count_apples = 0
+    count_oranges = 0
+  
+    for apple in apples:
+        final_position_apple = apple + a
+        if s <= final_position_apple <= t:
+            count_apples += 1
+    
+    for orange in oranges:
+        final_position_orange = orange + b
+        if s <= final_position_orange <= t:
+            count_oranges += 1
 
+    print(count_apples)
+    print(count_oranges)
 
-if __name__ == '__main__':
-    first_multiple_input = input().rstrip().split()
+s = 7
+t = 10
+a = 4
+b = 12
+apples = [2, 3, -4]
+oranges = [3, -2, -4]
 
-    s = int(first_multiple_input[0])
-
-    t = int(first_multiple_input[1])
-
-    second_multiple_input = input().rstrip().split()
-
-    a = int(second_multiple_input[0])
-
-    b = int(second_multiple_input[1])
-
-    third_multiple_input = input().rstrip().split()
-
-    m = int(third_multiple_input[0])
-
-    n = int(third_multiple_input[1])
-
-    apples = list(map(int, input().rstrip().split()))
-
-    oranges = list(map(int, input().rstrip().split()))
-
-    countApplesAndOranges(s, t, a, b, apples, oranges)
+countApplesAndOranges(s, t, a, b, apples, oranges)
