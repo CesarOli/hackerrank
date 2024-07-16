7028663acb2042a9f4bd7c3e4be2c7454474d56a
CesarOli <cesaraleoliveira@gmail.com>
2024-07-15 23:31:20 -0300

    feat: Initialize Number Line Jumps challenge with kangaroo function.

---
+++ Python/hackerrank/number_line_jumps.py
@@ -0,0 +1,19 @@
+
+def kangaroo(x1, v1, x2, v2):
+    return
+
+
+if __name__ == '__main__':
+
+    first_multiple_input = input().rstrip().split()
+
+    x1 = int(first_multiple_input[0])
+
+    v1 = int(first_multiple_input[1])
+
+    x2 = int(first_multiple_input[2])
+
+    v2 = int(first_multiple_input[3])
+
+    result = kangaroo(x1, v1, x2, v2)
+    
