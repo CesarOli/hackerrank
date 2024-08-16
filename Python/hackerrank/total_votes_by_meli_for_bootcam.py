ca2fbf255fa998bf8c95fcf0cdc36927be17bdd5
CesarOli <cesaraleoliveira@gmail.com>
2024-08-15 23:52:51 -0300

    feat: add API URL construction and initial variables for vote count.

--- Python/hackerrank/total_votes_by_meli_for_bootcam.py
+++ Python/hackerrank/total_votes_by_meli_for_bootcam.py
@@ -1,5 +1,10 @@
 def get_vote_count(city_name, estimated_cost):
+    
+    url = f"https://jsonmock.hackerrank.com/api/food_outlets?city={city_name}&estimated_cost={estimated_cost}"
+    total_votes = 0
+    page = 1 
 
+    
     while True:
 
         if 
