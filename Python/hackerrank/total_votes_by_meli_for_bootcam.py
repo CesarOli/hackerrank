acb0215d4654475c57d0853d65c508e0cba900d3
CesarOli <cesaraleoliveira@gmail.com>
2024-08-16 00:04:29 -0300

    feat: add pagination logic and vote count aggregation in get_vote_count function.

--- Python/hackerrank/total_votes_by_meli_for_bootcam.py
+++ Python/hackerrank/total_votes_by_meli_for_bootcam.py
@@ -3,16 +3,20 @@ def get_vote_count(city_name, estimated_cost):
     url = f"https://jsonmock.hackerrank.com/api/food_outlets?city={city_name}&estimated_cost={estimated_cost}"
     total_votes = 0
     page = 1 
-
     
     while True:
+        response = request.get(url + f'&page={page}')
+        data = response.json()
 
-        if 
+        if not data['data']:
+            break
 
-        for
+        for outlet in data['data']:
+            total_votes += outlet['user_rating']['vote']
+        
+        page += 1
+        if page > data['total_pages']:
+            break
+    return total_votes if total_votes > 0 else -1
 
-        if 
-
-
-    return
-if 
+if
