e4f11a82f5f9457e8c5bc76444262b3d911a1542
CesarOli <cesaraleoliveira@gmail.com>
2024-08-23 18:42:42 -0300

    Initial setup for divisibleSumPairs function challenge.

---
+++ Python/hackerrank/divisible_sum_pairs.py
@@ -0,0 +1,16 @@
+def divisibleSumPairs(n, k, ar):
+    
+    
+    return
+
+if __name__ == '__main__':
+
+    first_multiple_input = 0
+
+    n = int(first_multiple_input[0])
+
+    k = int(first_multiple_input[1])
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = divisibleSumPairs(n, k, ar)
