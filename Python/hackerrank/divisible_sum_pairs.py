3f8881d2e4e7df81587108c99cfea948019e2dbe
CesarOli <cesaraleoliveira@gmail.com>
2024-08-24 23:54:58 -0300

    Complete nested loops and finalize divisibleSumPairs function with input handling and output.

--- Python/hackerrank/divisible_sum_pairs.py
+++ Python/hackerrank/divisible_sum_pairs.py
@@ -53,20 +53,26 @@ def divisibleSumPairs(n, k, ar):
     count = 0
 
     for i in range(len(ar)):
+        for j in range(i + 1, len(ar)):
+            soma = ar[i] + ar[j]
+            if soma % k == 0:
+                count += 1
     
-    
-            return count 
+    return count 
 
 if __name__ == '__main__':
 
     statement()
 
-    first_multiple_input = 0
+    first_multiple_input = input().rstrip().split()
 
-    n = int(first_multiple_input[0])
+    n = int(input(first_multiple_input[0]))
 
     k = int(first_multiple_input[1])
 
     ar = list(map(int, input().rstrip().split()))
 
     result = divisibleSumPairs(n, k, ar)
+
+    print(result)
+    
