7056b54a166b3238c59c45167b70af3575ee37df
CesarOli <cesaraleoliveira@gmail.com>
2024-08-25 00:02:52 -0300

    Initial setup for migratoryBirds function challenge.

---
+++ Python/hackerrank/migratory_birds.py
@@ -0,0 +1,13 @@
+
+def migratoryBirds(arr):
+    return 
+
+
+if __name__ == '__main__':
+
+    arr_count = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = migratoryBirds(arr)
+    
